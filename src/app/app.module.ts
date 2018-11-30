e2b4aeebe262feaa8df6d9a4c6ca30b00d1fe7ff
sumit <mishrasumit042@gmail.com>
2018-11-30 19:09:24 +0530

    modified UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { RegisterUserComponent } from './register-user/register-user.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { HomePageComponent } from './User/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterUserComponent,
     LoginPageComponent,
-    ForgotPasswordComponent
+    ForgotPasswordComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule
